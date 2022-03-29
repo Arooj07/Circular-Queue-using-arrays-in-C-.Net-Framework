1fc67bed18cf97459f5cf2ba8424e601415666e8
Arooj07 <100277795+Arooj07@users.noreply.github.com>
2022-03-29 19:41:24 +0500

    Add files via upload

---
+++ CircularQueue.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircularQueue
+{
+    internal class CircularQueue
+    {
+        private int [] arr;
+        private int front;
+        private int rear;
+        private int last;
+        private int count;
+
+        public CircularQueue(int len)
+        {
+            arr = new int[len];
+            front = 0;
+            rear = -1;
+            last = len;
+            count = 0;
+        }
+
+        public void Enqueue(int num)
+        {
+            if (count == last)
+            {
+                Console.WriteLine("Queue is Completely Filled");
+                return;
+            }
+            else
+            {
+                rear = (rear + 1) % last;
+                arr[rear] = num;
+
+                count++;
+                Console.WriteLine("---------------------------------------------");
+                Console.WriteLine("Current Length of Circular Queue is: " + count);
+                
+            }
+        }
+
+        public void Dequeue()
+        {
+            if (count == 0)
+            {
+                Console.WriteLine("Queue is Empty having no any Element");
+            }
+            else
+            {
+                Console.WriteLine("Deleted element from the Queue is: " + arr[front]);
+
+                front = (front + 1) % last;
+
+                count--;
+                Console.WriteLine("---------------------------------------------");
+                Console.WriteLine("Current Length of Circular Queue is: " + count);
+                Console.WriteLine("---------------------------------------------");
+            }
+        }
+
+        public void display()
+        {
+            int i = 0;
+            int j = 0;
+
+            if (count == 0)
+            {
+                Console.WriteLine("Queue have no Element - It is Empty");
+                return;
+            }
+            else
+            {
+                for (i = front; j < count;)
+                {
+                    Console.WriteLine(arr[i]);
+
+                    i = (i + 1) % last;
+                    j++;
+
+                }
+                Console.WriteLine("---------------------------------------------");
+                Console.WriteLine("Current Size of CircularQueue is: " + count);
+                
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircularQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CircularQueue Q = new CircularQueue(10);
+            while (true)
+            {
+                int a = 0;
+                Console.WriteLine("Enter Number to perform specific Operation");
+                Console.WriteLine("Enter 1 to add a number in Queue");
+                Console.WriteLine("Enter 2 to delete a number from Queue");
+                Console.WriteLine("Enter 3 to display the elements of Queue");
+                a = Convert.ToInt32(Console.ReadLine());
+                switch (a)
+                {
+                    case 1:
+                        int b = 0;
+                        Console.WriteLine("Enter Number to add in Circular Queue");
+                        b = Convert.ToInt32(Console.ReadLine());
+                        Q.Enqueue(b);
+                        Console.WriteLine("-------------------------------------");
+                        break;
+                    case 2:
+                        Q.Dequeue();
+                        Console.WriteLine("-------------------------------------");
+                        break;
+                    case 3:
+                        Console.WriteLine("Elements in CircularQueue are: ");
+                        Q.display();
+                        Console.WriteLine("-------------------------------------");
+                        break;
+                    default:
+                        Console.WriteLine("You can Try Again :)");
+                        break;
+                }
+            }
+          
+        }
+    }
+}
